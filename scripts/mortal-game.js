ead9c94d280d6d50aba29136a11376a1f6a50a4a
Luke <luke.nickerson@gmail.com>
2021-01-27 23:44:32 -0500

    Removed all dependencies

--- scripts/mortal-game.js
+++ scripts/mortal-game.js
@@ -1,13 +1,10 @@
-RocketBoots.loadComponents([
-	"Game",
-	"StateMachine"
-]).ready(function(){
+document.addEventListener('DOMContentLoaded', function(){
 
-	const g = new RocketBoots.Game({
+	const g = {
 		name: "Mortal",
-		instantiateComponents: [],
-		version: "v1.0.0"
-	});	
+		version: "v1.1.0" // Now with 100% less jQuery and 100% less rocketboots
+	};
+	console.log(g);
 
 	g.age = 0;
 	g.rate = 1;
@@ -16,7 +13,12 @@ RocketBoots.loadComponents([
 	updateUI();
 	updateGraphic(getYears());
 
-	$('button.age').click(increment);
+	document.addEventListener('click', function(e) {
+		if (!e.target.closest('.age-button')) { return; }
+		increment();
+	});
+
+	function $(selector) { return document.querySelector(selector); }
 
 	function increment() {
 		g.age += Math.round(g.rate);
@@ -28,21 +30,20 @@ RocketBoots.loadComponents([
 			updateGraphic(year);
 		}
 		if (year >= 99) {
-			$('.clickers').hide();
+			$('.clickers').style.display = 'none';
 		}		
 	}
 
 	function updateGraphic(year) {
 		const i = getGraphicIndex(year);
-		$('.character')
-			.removeClass('age-0 age-1 age-2 age-3 age-4 age-5 age-6')
-			.addClass('age-' + i);
+		$('.character').classList.remove('age-0', 'age-1', 'age-2', 'age-3', 'age-4', 'age-5', 'age-6');
+		$('.character').classList.add('age-' + i);
 	}
 
 	function updateUI() {
-		$('.age-years').html(getYears());
-		$('.age-days').html(g.age.toLocaleString());
-		$('.rate').html(g.rate.toLocaleString());
+		$('.age-years').innerHTML = getYears();
+		$('.age-days').innerHTML = g.age.toLocaleString();
+		$('.rate').innerHTML = g.rate.toLocaleString();
 	}
 
 	function getYears() {
@@ -58,7 +59,7 @@ RocketBoots.loadComponents([
 			return 2;
 		} else if (year < 22) {
 			return 3;
-		} else if (year < 40) {
+		} else if (year < 43) {
 			return 4;
 		} else if (year < 70) {
 			return 5;
@@ -67,5 +68,4 @@ RocketBoots.loadComponents([
 		}
 		return 7;
 	}
-
-}).init();
+});
